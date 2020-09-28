9a6f3e815804ec3f7bc0cdea8d0b98faed5b5802
Jorge <jorgediazok@gmail.com>
2020-09-27 21:11:05 -0300

    adding css to the carousel

--- index.js
+++ index.js
@@ -6,3 +6,15 @@ const navbarLinks = document.getElementsByClassName('navbar-links')[0];
 toggleButton.addEventListener('click', () => {
   navbarLinks.classList.toggle('active');
 });
+
+//lightslider
+
+$(document).ready(function () {
+  $('#autoWidth').lightSlider({
+    autoWidth: true,
+    loop: true,
+    onSliderLoad: function () {
+      $('autoWidth').removeClass('cs-hidden');
+    },
+  });
+});
